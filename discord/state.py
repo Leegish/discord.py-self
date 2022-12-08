ea985ed96f04a9c55b575f26e67a148d1eab35a4
Legish <null>
2022-12-08 10:45:35 +0200

    add on_raw_typing back

--- discord/state.py
+++ discord/state.py
@@ -2170,6 +2170,8 @@ class ConnectionState:
             asyncio.create_task(logging_coroutine(coro, info='Voice Protocol voice server update handler'))
 
     def parse_typing_start(self, data: gw.TypingStartEvent) -> None:
+        self.dispatch('raw_typing', data) # why remov?
+
         channel, guild = self._get_guild_channel(data)
         if channel is not None:
             member = None
